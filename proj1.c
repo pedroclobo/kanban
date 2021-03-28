cb217677e465cdc854848786da6d5c57ff93f4de
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-03-28 23:10:59 +0100

    Functional t command.

--- proj1.c
+++ proj1.c
@@ -1,16 +1,35 @@
 # include <stdio.h>
 
-# define MAX_CMD 100
+/* Max value allowed */
+# define TASK 10000
+# define USER 50
+
+/* Max structural values allowed */
+# define CMD_LEN 100
+# define TASK_LEN 50
+# define ACT_LEN 20
+# define USER_LEN 20
 
 /* Define the key functions */
 # define EXIT 'q'
 # define NEW_TASK 't'
+# define LIST_TASK 'l'
+
+/* Define task */
+typedef struct {
+    unsigned int id;
+    char *des;
+    char *user;
+    char *act;
+    unsigned int dur;
+    unsigned int exec_time;
+} task;
 
 /* Prompts the user for a command to
  * execute and saves it to a string */
 void get_cmd(char cmd[]) {
     printf("? ");
-    scanf("%s", cmd);
+    fgets(cmd, CMD_LEN, stdin);
 }
 
 /* Returns the key that matches the
@@ -21,17 +40,42 @@ char get_key(char s[]) {
 
 /* Adds a new task to the system,
  * printing the task id */
-void new_task(int duration, char[]);
+void new_task(char cmd[], unsigned int *task_c, task tasks[]) {
+    unsigned int dur;
+    char des[100];
+
+    sscanf(cmd, "f %u %[^\n]", &dur, des);
+
+    tasks[*task_c].dur = dur;
+    tasks[*task_c].des = des;
+    *task_c += 1;
+
+    printf("task %d\n", *task_c);
+}
+
+void list_tasks(int ids[]);
+
+void foward_time(int dur);
+
+void add_user(char user[]);
+
+void move_taks(int id, char user[], char act[]);
 
 int main() {
     /* Declare command string */
-    char cmd[MAX_CMD];
+    char cmd[CMD_LEN];
+
+    /* Tasks */
+    task tasks[TASK];
+    unsigned int task_c = 0;
 
     /* While user doesn't quit the program */
-    for (get_cmd(cmd); get_key(cmd) != EXIT; get_cmd(cmd))
-
-        switch (get_key(cmd)) {
+    for (get_cmd(cmd); get_key(cmd) != EXIT; get_cmd(cmd)) {
+        switch(get_key(cmd)) {
+            case NEW_TASK:
+                new_task(cmd, &task_c, tasks);
         }
+    }
 
     return 0;
 }
