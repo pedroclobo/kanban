bc8c329f78ade211255f6163787688a9cc80a6e2
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-03-30 11:57:21 +0100

    Functional t and l command.

--- proj1.c
+++ proj1.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <string.h>
 
 /* Max value allowed */
 # define TASK 10000
@@ -18,9 +19,9 @@
 /* Define task */
 typedef struct {
     unsigned int id;
-    char *des;
-    char *user;
-    char *act;
+    char des[TASK_LEN];
+    char user[USER_LEN];
+    char act[ACT_LEN];
     unsigned int dur;
     unsigned int exec_time;
 } task;
@@ -28,7 +29,6 @@ typedef struct {
 /* Prompts the user for a command to
  * execute and saves it to a string */
 void get_cmd(char cmd[]) {
-    printf("? ");
     fgets(cmd, CMD_LEN, stdin);
 }
 
@@ -41,19 +41,48 @@ char get_key(char s[]) {
 /* Adds a new task to the system,
  * printing the task id */
 void new_task(char cmd[], unsigned int *task_c, task tasks[]) {
-    unsigned int dur;
-    char des[100];
+    char act[ACT_LEN] = "TO DO";
 
-    sscanf(cmd, "f %u %[^\n]", &dur, des);
+    /* If task number limit is exceed */
+    if (*task_c + 1 >= TASK)
+        printf("too many tasks\n");
 
-    tasks[*task_c].dur = dur;
-    tasks[*task_c].des = des;
-    *task_c += 1;
-
-    printf("task %d\n", *task_c);
+    /* Else */
+    else {
+        sscanf(cmd, "%*1c %u %[^\n]", &tasks[*task_c].dur, tasks[*task_c].des);
+        sscanf(act, "%[^\n]", tasks[*task_c].act);
+        *task_c += 1;
+        printf("task %u\n", *task_c);
+    }
 }
 
-void list_tasks(int ids[]);
+void list_tasks(char cmd[], task tasks[], unsigned int *task_c) {
+    unsigned int ids[26];
+    unsigned int i, j, number = 0;
+
+    /* Only l */
+    if (!strcmp(cmd, "l\n"))
+        for (i = 0; i < *task_c; i++)
+            printf("%u %s #%u %s\n", i+1, tasks[i].act, tasks[i].dur, tasks[i].des);
+
+    else {
+        sscanf(cmd, "%*2c %[^EOF]", cmd);
+
+        for (i = 0, j = 0; cmd[i] != '\0'; i++) {
+            if (cmd[i] >= '0' && cmd[i] <= '9')
+                number = 10*number + (cmd[i] - '0');
+            else if (cmd[i] == ' '){
+                ids[j++] = number;
+                number = 0;
+            }
+        }
+        ids[j++] = number;
+
+        for (i = 0; i < j; i++)
+            printf("%u %s #%u %s\n", ids[i], tasks[ids[i]-1].act, tasks[ids[i]-1].dur, tasks[ids[i]-1].des);
+    }
+}
+
 
 void foward_time(int dur);
 
@@ -74,6 +103,10 @@ int main() {
         switch(get_key(cmd)) {
             case NEW_TASK:
                 new_task(cmd, &task_c, tasks);
+                break;
+            case LIST_TASK:
+                list_tasks(cmd, tasks, &task_c);
+                break;
         }
     }
 
